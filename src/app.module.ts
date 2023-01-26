fb209bd091b68afb177cf27d6979bdf07ac701c1
Anbarasan <anbarasanj24@gmail.com>
2023-01-26 11:51:19 +0530

    Added User Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { AppService } from './app.service';
       synchronize: true,
       autoLoadEntities: true,
       url : process.env.DATABASE_URL,
-    })
+    }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
